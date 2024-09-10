4a1301cd157021a86fe143ff45e9c02ee6b09177
Dheeraj <vishwakarmadheeraj2002@gmail.com>
2024-09-10 22:36:28 +0530

    done successfully

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(props) {
+    return (
+        <div className="footer">
+            <p>Ⓒ Programmig with Dheeraj</p>
+        </div>
+    );
+}
+
+export default Footer;
